581c384dcf98756e0fe02874aee803f70e21c7c3
Marcin Tatoń <tatonmarcinpr@gmail.com>
2023-07-13 14:39:38 +0200

    auth interceptop refactor

--- client/src/app/interceptors/authorization.interceptor.ts
+++ client/src/app/interceptors/authorization.interceptor.ts
@@ -6,28 +6,29 @@ import {
   HttpInterceptor,
   HttpHeaders,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import User from '../types/interfaces/user';
+import { AccountService } from '../services/account.service';
 
 @Injectable()
 export class AuthorizaionInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private accountService: AccountService) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    const userString = localStorage.getItem('user');
-    if (!userString) return next.handle(request);
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: (user) => {
+        if (user)
+          request = request.clone({
+            setHeaders: {
+              Authorization: 'Bearer ' + user.token,
+            },
+          });
+      },
+    });
 
-    const user = JSON.parse(userString!) as User;
-
-    return next.handle(
-      request.clone({
-        setHeaders: {
-          Authorization: 'Bearer ' + user.token,
-        },
-      })
-    );
+    return next.handle(request);
   }
 }
